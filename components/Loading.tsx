eb7a6a77505b4cd1298bf5c2b8b1d73bb525b1c1
tosankrisna <tosankrisna@gmail.com>
2024-12-05 00:30:48 +0800

    feat: loading component

---
+++ components/Loading.tsx
@@ -0,0 +1,7 @@
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+
+const Loading = () => {
+  return <DotLottieReact src="/loading.lottie" loop autoplay />;
+};
+
+export default Loading;
