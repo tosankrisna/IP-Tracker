42cd588c0e936905620aaa7bb11b61e8802e1d9e
tosankrisna <tosankrisna@gmail.com>
2024-12-05 00:49:44 +0800

    refactor: add container

--- components/Error.tsx
+++ components/Error.tsx
@@ -1,7 +1,13 @@
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 const Error = () => {
-  return <DotLottieReact src="/error.lottie" loop autoplay />;
+  return (
+    <div className="flex justify-center pt-36">
+      <div className="w-1/2">
+        <DotLottieReact src="/error.lottie" loop autoplay />
+      </div>
+    </div>
+  );
 };
 
 export default Error;
--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,7 +1,13 @@
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 const Loading = () => {
-  return <DotLottieReact src="/loading.lottie" loop autoplay />;
+  return (
+    <div className="flex justify-center pt-36">
+      <div className="w-1/2">
+        <DotLottieReact src="/loading.lottie" loop autoplay />
+      </div>
+    </div>
+  );
 };
 
 export default Loading;
