e7e6ac20c4a29ccf0f7fd51d4d2a45b5ee547677
tosankrisna <tosankrisna@gmail.com>
2024-12-05 00:31:14 +0800

    feat: update information

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,23 @@
+"use client";
+
+import Error from "@/components/Error";
 import Hero from "@/components/Hero";
 import Information from "@/components/Information";
 import Input from "@/components/Input";
+import Loading from "@/components/Loading";
+import { useFetch } from "@/hooks/useFetch";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  const { data, error, isLoading } = useFetch("");
+
+  if (isLoading) return <Loading />;
+  if (error) return <Error />;
+  if (data.code === 422) {
+    alert(data.messages);
+    redirect("/");
+  }
+
   return (
     <main className="relative flex flex-col">
       <Hero>
@@ -11,7 +26,7 @@ export default function Home() {
         </h1>
         <Input />
       </Hero>
-      <Information />
+      <Information data={data} />
     </main>
   );
 }
--- components/Information.tsx
+++ components/Information.tsx
@@ -1,4 +1,21 @@
-const Information = () => {
+type Location = {
+  region: string;
+  timezone: string;
+};
+
+type InformationProps = {
+  ip: string;
+  isp: string;
+  location: Location;
+};
+
+const Information = ({ data }: { data: InformationProps }) => {
+  const {
+    ip,
+    isp,
+    location: { region, timezone },
+  } = data;
+
   return (
     <div className="z-10 absolute top-40 w-full">
       <div className="flex justify-center">
@@ -7,25 +24,27 @@ const Information = () => {
             <span className="uppercase text-xs md:text-sm lg:text-md mb-2 text-slate-600">
               IP Address
             </span>
-            <span className="font-bold text-black"></span>
+            <span className="font-bold text-black">{ip}</span>
           </div>
           <div className="flex flex-col">
             <span className="uppercase text-xs md:text-sm lg:text-md mb-2 text-slate-600">
               Location
             </span>
-            <span className="font-bold text-black"></span>
+            <span className="font-bold text-black">{region}</span>
           </div>
           <div className="flex flex-col">
             <span className="uppercase text-xs md:text-sm lg:text-md mb-2 text-slate-600">
               Timezone
             </span>
-            <span className="font-bold text-black"></span>
+            <span className="font-bold text-black">UTC {timezone}</span>
           </div>
           <div className="flex flex-col">
             <span className="uppercase text-xs md:text-sm lg:text-md mb-2 text-slate-600">
               ISP
             </span>
-            <span className="font-bold text-black text-sm lg:text-lg"></span>
+            <span className="font-bold text-black text-sm lg:text-lg">
+              {isp}
+            </span>
           </div>
         </div>
       </div>
